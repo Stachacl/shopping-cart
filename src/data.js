5e3a9b1a7af3f46e5a0e6ef79fca3fe0b902b805
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-02 15:24:55 +1000

    items description changed

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 export default [
   {
     id: 1,
-    title: 'Samsung Galaxy S7',
-    price: 599.99,
+    title: 'Perfume',
+    price: 129.00,
     img:
       'https://ik.imagekit.io/stcl/Shop/pink-perfume_li0OeFpuj.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662095156582',
     amount: 1,
   },
   {
     id: 2,
-    title: 'google pixel ',
+    title: 'Headphones ',
     price: 499.99,
     img:
       'https://ik.imagekit.io/stcl/Shop/pink-headphones_39_w-SINT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662095156643',
@@ -17,8 +17,8 @@ export default [
   },
   {
     id: 3,
-    title: 'Xiaomi Redmi Note 2',
-    price: 699.99,
+    title: 'Body lotion',
+    price: 35.50,
     img:
       'https://ik.imagekit.io/stcl/Shop/Pink-bottle_-DjaMA4bB.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662095156603',
     amount: 1,
