99f7ed240349dce9e0fdcbdf4f6df0145486d0be
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-02 21:53:29 +1000

    set remove item functionality in Reduser.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,17 @@
-const reducer = (state,action) => {
-    //take all the values from the current state and only change cart value to the empty array:
-    if(action.type === 'CLEAR_CART') {
-        return {...state,cart:[]}
-    }
-    if(action.type === 'REMOVE') {
-        return {...state, cart:state.cart.filter((cartItem) => cartItem.id !== action.payload),}
-    }
-    return state
-}
+const reducer = (state, action) => {
+  //take all the values from the current state and only change cart value to the empty array:
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  return state;
+};
 
 //as an argument it is looking at the state  and action
 
-export default reducer
+export default reducer;
