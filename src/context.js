e572ecaf7871498dc80f7fcdd58693f017a765bd
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-02 21:37:25 +1000

    passed clearCart function to CartContainer.js  to the clear button

--- src/context.js
+++ src/context.js
@@ -1,40 +1,41 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = '-useReducer-cart-projecthttps://course-api.com/react'
-const AppContext = React.createContext()
+const url = "-useReducer-cart-projecthttps://course-api.com/react";
+const AppContext = React.createContext();
 
 //setting variable initialState as object:
 const initialState = {
   loading: false,
-  cart:cartItems,
-  total:0,
-  amount:0,
-}
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 
 //custom hook:
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const clearCart = () => {
-    dispatch({type:'CLEAR_CART'})
-  }
+    dispatch({ type: "CLEAR_CART" });
+  };
 
   return (
     <AppContext.Provider
       value={{
-       ...state,
+        ...state,
+        clearCart,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
