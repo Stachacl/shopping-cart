6152ac307aef34db1a4f5e889e0d3617c069efa7
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-06 14:41:28 +1000

    total amount value established

--- src/reducer.js
+++ src/reducer.js
@@ -19,13 +19,31 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
   if (action.type === "DECREASE") {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount - 1 };
-      }
-      return cartItem;
-    }).filter((cartItem)=> cartItem.amount !== 0)
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
     return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    //setting a call back function:
+    const { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        cartTotal.amount += amount; //means that whatewer the value add the amount property value
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+
+    return { ...state, total, amount };
   }
   return state;
 };
